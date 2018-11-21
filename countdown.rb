b956b34318973d4cb214f23df3e46ca563bc7010
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-21 08:31:12 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,12 +11,4 @@ def countdown(num)
 end
 
 
-# def countdown_with_sleep(number)
-#   counter = 0
-#   while counter < number
-#     puts "#{number} SECOND(S)!"
-#     sleep 5
-#     counter+=1
-#   end
-#   return "HAPPY NEW YEAR!"
-# end
+
